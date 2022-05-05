7abb2e71b6812edafe7ff3d3ce0343717c0a835b
Sue Roberts <suerobertstx@yahoo.com>
2022-05-05 13:41:11 -0500

    Pass: Test 4

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,10 @@ function Pizza(size, topping){
 Pizza.prototype.pizzaPrice = function() {
   let pizzaPrice = 10;
   if (this.size==="small") { 
-    pizzaPrice += 1 ;
+    pizzaPrice += 1;
 } else if (this.size==="medium") {
-    pizzaPrice += 2
-} console.log(pizzaPrice);
-
-
+    pizzaPrice += 2;
+} else if (this.size==="large") {
+  pizzaPrice += 4;
+}console.log(pizzaPrice);
 }
