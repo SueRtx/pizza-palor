5b4ad98cb37d7158992b293457ea68520efa47b8
Sue Roberts <suerobertstx@yahoo.com>
2022-05-06 02:53:36 -0500

    Add image of pizza background and style font colors & location.

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ Pizza.prototype.pizzaPrice = function() {
 
 function displayPizzaOrder(pizzaToDisplay) {
   let pizzaorder = $("p#output");
-  let htmlForPizzaOrder = "Your order: " + pizzaToDisplay.size + "pizza with " + pizzaToDisplay.topping + " topping" + " " + "Total: $" + pizzaToDisplay.pizzaPrice();
+  let htmlForPizzaOrder = "Your order:" + " " + pizzaToDisplay.size + " " + "pizza with " + pizzaToDisplay.topping + " topping." + " " + "Total: $" + pizzaToDisplay.pizzaPrice();
   pizzaorder.html(htmlForPizzaOrder);
 }
 
@@ -31,6 +31,7 @@ $(document).ready(function () {
     let newPizza = new Pizza(newSize,newTopping);
     let finalPizzaTotal = newPizza.pizzaPrice();
     displayPizzaOrder(newPizza);
+    ("#output").show();
   });
 });    
 
