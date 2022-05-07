5f06e54be98b0f4fe0a60619320b5000eb2a885a
Sue Roberts <suerobertstx@yahoo.com>
2022-05-06 19:05:06 -0500

    FIX: Indentation, spacing, and code structure.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function Pizza(size, topping){
+function Pizza(size, topping) {
   this.size = size;
   this.topping = topping;
 }
@@ -13,7 +13,8 @@ Pizza.prototype.pizzaPrice = function() {
     pizzaPriceTotal += this.topping *3 + 5;
   } else if (this.size==="Stuffed") {
     pizzaPriceTotal += this.topping *3 + 6;
-  } return (pizzaPriceTotal);
+  } 
+  return (pizzaPriceTotal);
 }
 
 function displayPizzaOrder(pizzaToDisplay) {
@@ -22,14 +23,13 @@ function displayPizzaOrder(pizzaToDisplay) {
   pizzaorder.html(htmlForPizzaOrder);
 }
 
-
 $(document).ready(function () {
   $("form#pizza").submit(function (event) {
     event.preventDefault();
     let newTopping = [];
     let newSize = $("input:radio[name=size]:checked").val();
     newTopping = $('input[type="checkbox"]:checked').length;
-    let newPizza = new Pizza(newSize,newTopping);
+    let newPizza = new Pizza(newSize, newTopping);
     let finalPizzaTotal = newPizza.pizzaPrice();
     displayPizzaOrder(newPizza);
   });
